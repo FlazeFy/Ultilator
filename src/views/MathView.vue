e4bbe60c822c28289719511801a5cc7f042c00f9
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-01-19 15:05:52 +0700

    simple math
    sum, subs, multiplication, div, lcm, gcd, pythagoras. not finished

--- src/views/MathView.vue
+++ src/views/MathView.vue
@@ -1,5 +1,10 @@
+<script setup>
+  import SimpleMath from "../components/content/math/simpleMath.vue"
+</script>
+
 <template>
   <div>
-    <h2>Math</h2>
+    <h2 class="text-primary brand">Math</h2>
+    <SimpleMath></SimpleMath>
   </div>
 </template>
