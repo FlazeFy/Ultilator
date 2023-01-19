50ff7b88e300144fe75250bc13920cdd28f4c3bd
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-01-19 21:17:08 +0700

    temperature converter
    and some input validator

--- src/components/content/math/simpleMath.vue
+++ src/components/content/math/simpleMath.vue
@@ -1,44 +1,68 @@
 <script>
-    function countSimpleMath(method) {
-        a = document.getElementById("number_a_simpleMath").value;
-        b = document.getElementById("number_b_simpleMath").value;
+    export default {
+        data() {
+            return {
+                name: 'Vue.js'
+            }
+        },
+        methods: {
+            countSimpleMath(method) {
+                let a = parseInt(document.getElementById("number_a_simpleMath").value);
+                let b = parseInt(document.getElementById("number_b_simpleMath").value);
+                let a_style = document.getElementById("number_a_simpleMath").style;
+                let b_style = document.getElementById("number_b_simpleMath").style;
+                var res
 
-        if(method == "py"){
-			res = Math.sqrt(a*a + b*b);
-        } else if(method == "+"){
-            res = (a + b);
-        } else if(method == "-"){
-            res = (a - b);
-        } else if(method == "*"){
-            res = (a * b);
-        } else if(method == "/"){
-            res = (a / b);
-        } else if(method == "mod"){
-            res = (a + b);
-        } else if(method == "gcd"){
-            a = Math.abs(a);
-			b = Math.abs(b);
-			while(b) {
-				var c = b;
-				b = a % b;
-				a = c;
-			}
-            res = a;
-        } else if(method == "lcm"){
-            a = Math.abs(a);
-			b = Math.abs(b);
-			function gcd_2(){
-				while(b) {
-					var c = b;
-					b = a % b;
-					a = c;
-				}
-				return a;
-			}
-			res = Math.abs((a * b) / gcd_2(a, b));
+                //Check input value
+                if(isNaN(a)){
+                    a_style.border = "2px solid #3EAF7C";
+                    res = "-";
+                } else if(isNaN(b)){
+                    b_style.border = "2px solid #3EAF7C";
+                    res = "-";
+                } else {
+                    a_style.border = "none";
+                    b_style.border = "none";
+
+                    if(method == "py"){
+                        res = Math.sqrt(a*a + b*b);
+                    } else if(method == "+"){
+                        res = (a + b);
+                    } else if(method == "-"){
+                        res = (a - b);
+                    } else if(method == "*"){
+                        res = (a * b);
+                    } else if(method == "/"){
+                        res = (a / b);
+                    } else if(method == "mod"){
+                        res = (a + b);
+                    } else if(method == "gcd"){
+                        a = Math.abs(a);
+                        b = Math.abs(b);
+                        while(b) {
+                            var c = b;
+                            b = a % b;
+                            a = c;
+                        }
+                        res = a;
+                    } else if(method == "lcm"){
+                        a = Math.abs(a);
+                        b = Math.abs(b);
+                        function gcd_2(){
+                            while(b) {
+                                var c = b;
+                                b = a % b;
+                                a = c;
+                            }
+                            return a;
+                        }
+                        res = Math.abs((a * b) / gcd_2(a, b));
+                    }
+                }
+
+                document.getElementById("result_simpleMath").value = res;
+            }
         }
-
-        document.getElementById("result_simpleMath").value = Math.sqrt(res);
     }
 </script>
 
@@ -60,7 +84,7 @@
             </div>
             <div class="col-lg-4 col-sm-6">
                 <div class="form-floating">
-                    <input type="number" class="form-control" id="result_simpleMath" disabled>
+                    <input type="text" class="form-control" id="result_simpleMath" disabled>
                     <label for="floatingInput">Result</label>
                 </div>
             </div>
