21dfc9dc75c6db24036673a3e31f9f9ba2e26d22
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-01-23 11:09:56 +0700

    count area of cube, cuboid, and sphere

--- src/views/MathView.vue
+++ src/views/MathView.vue
@@ -1,12 +1,14 @@
 <script setup>
   import SimpleMath from "../components/content/math/simpleMath.vue"
   import TwoDGeo from "../components/content/math/2dgeometry.vue"
+  import ThreeDGeo from "../components/content/math/3dgeometry.vue"
 </script>
 
 <template>
   <div>
     <h2 class="text-primary brand">Math</h2>
     <SimpleMath class="mb-4"></SimpleMath>
-    <TwoDGeo></TwoDGeo>
+    <TwoDGeo class="mb-4"></TwoDGeo>
+    <ThreeDGeo></ThreeDGeo>
   </div>
 </template>
